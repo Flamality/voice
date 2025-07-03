940c7ac99ecc9030582d17ebe9daa46cf392013e
Remi <urflamality@gmail.com>
2025-07-03 00:26:07 -0400

    Added base questionnaire page

---
+++ src/frontend/questionnaire/Questionnaire.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Questionnaire() {
+  return (
+    <div className='questionnaire-page'>
+      <div className='questionnaire-header'>
+        <h1>Questionnaire</h1>
+      </div>
+      <div className='questionnaire-content'></div>
+    </div>
+  );
+}
